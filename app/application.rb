e2de22326e2449e58b01164d7a5e30a59f3290f2
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-23 02:25:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,17 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
- 
-    
- 
-    if time.now <= 12:00
+
+
+
+    if time.now (<= 12:00:00)
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
     end
- 
+
     resp.finish
   end
- 
+
 end
